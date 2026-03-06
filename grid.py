ec2b8d2d9bf25ac35eb4a1a6212bb806a6ef9f85
PrithuAshish <prithu.ashish2025@vitstudent.ac.in>
2026-03-06 11:44:13 +0530

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,1 +1,22 @@
+import numpy as np
+import random
 
+class GridEnvironment:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = np.zeros((height, width))
+
+    def add_obstacle(self, x, y):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.grid[y][x] = 1
+
+    def generate_random_obstacles(self, count):
+
+        for _ in range(count):
+
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+
+            self.add_obstacle(x, y)
