b64301a5c84dc360f28fab1eb2780a326da5a40b
PrithuAshish <prithu.ashish2025@vitstudent.ac.in>
2026-03-06 11:45:53 +0530

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
 
+def show_grid(grid, path=None, start=None, goal=None):
+
+    plt.imshow(grid, cmap="gray_r")
+
+    if path:
+
+        xs = [p[0] for p in path]
+        ys = [p[1] for p in path]
+
+        plt.plot(xs, ys)
+
+    if start:
+        plt.scatter(start[0], start[1])
+
+    if goal:
+        plt.scatter(goal[0], goal[1])
+
+    plt.title("Drone Flight Path Simulator")
+    plt.show()
